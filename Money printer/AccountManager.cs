86069aa3df2afcf614a71d1b88a4536ad9db1631
Layla Perso <richardlayla@hotmail.com>
2024-01-02 16:55:49 +0100

    ignore bin and debug create account

--- Money printer/AccountManager.cs
+++ Money printer/AccountManager.cs
@@ -6,4 +6,20 @@ public class AccountManager
     {
         return AccountRepo.Accounts;
     }
+
+    public int CreateAccount(int clientId, double balance)
+    {
+        try
+        {
+            var newAccount = new Account(clientId, balance);
+            AccountRepo.Accounts.Add(newAccount);
+            return newAccount.Id;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return -1;
+        }
+        
+    }
 }
--- Money printer/Dashboard.cs
+++ Money printer/Dashboard.cs
@@ -42,13 +42,13 @@ public class Dashboard
                 DisplayClient();
                 break;
             case 2:
-                 DisplayAccounts();
+                DisplayAccounts();
                 break;
             case 3:
                 NewClient();
                 break;
             case 4:
-                // NewAccount()
+                NewAccount();
                 break;
             case 5:
                 Console.WriteLine("Bye ...");
@@ -64,22 +64,22 @@ public class Dashboard
     {
         var clients = ClientManager.GetClients();
         Printer.PrintLine();
-        Printer.PrintRow("ClientId","Name", "Joined at", "Accounts Ids", "Accounts Names");
-        clients.ForEach(c => { Printer.PrintRow(c.Id.ToString(),c.Name, c.DateJoined.ToShortDateString(), "", ""); });
+        Printer.PrintRow("ClientId", "Name", "Joined at", "Accounts Ids", "Accounts Names");
+        clients.ForEach(c => { Printer.PrintRow(c.Id.ToString(), c.Name, c.DateJoined.ToShortDateString(), "", ""); });
         Printer.PrintLine();
         Console.ReadLine();
         Start();
-    }    
-    
+    }
+
     private void DisplayAccounts()
     {
         var accounts = AccountManager.GetAccounts();
         Printer.PrintLine();
-        Printer.PrintRow("Account Id","Client Id","Client Name", "Balance");
+        Printer.PrintRow("Account Id", "Client Id", "Client Name", "Balance");
         accounts.ForEach(a =>
         {
             var clientName = GetClientName(a.ClientId);
-            Printer.PrintRow(a.Id.ToString(), a.ClientId.ToString(),clientName, a.Balance.ToString());
+            Printer.PrintRow(a.Id.ToString(), a.ClientId.ToString(), clientName, a.Balance.ToString());
         });
         Printer.PrintLine();
         Console.ReadLine();
@@ -88,7 +88,6 @@ public class Dashboard
 
     private string GetClientName(int clientId)
     {
-        
         try
         {
             return ClientManager.GetClientById(clientId).Name;
@@ -102,30 +101,93 @@ public class Dashboard
 
     private void NewClient()
     {
-        var nameInput ="";
-        do
-        {
-            Console.WriteLine("Enter the name of the Client : ");
-            nameInput = Console.ReadLine();
-        } while (!IsNameValid(nameInput));        
-        
-        var joinedAt = DateTime.UtcNow;
-        var isJoinValid=true;
-        do
-        {
-            Console.WriteLine("Enter the date when the client joined the bank. Press Enter if now : ");
-            var joinedAtInput = Console.ReadLine();
-            if(joinedAtInput != null && joinedAtInput.Trim().Length!=0)
-                isJoinValid =  DateTime.TryParse(joinedAtInput, out joinedAt);
-        } while (!isJoinValid);
+        var nameInput = AskName();
+        var joinedAt = AskJoinDate();
 
         var newClient = ClientManager.CreateClient(nameInput, joinedAt);
         Console.WriteLine("Client created. Id : " + newClient);
         DisplayClient();
     }
 
+    private DateTime AskJoinDate()
+    {
+        var res = DateTime.UtcNow;
+        var isJoinValid = true;
+        do
+        {
+            Console.WriteLine("Enter the date when the client joined the bank. Press Enter if now : ");
+            var joinedAtInput = Console.ReadLine();
+            if (joinedAtInput != null && joinedAtInput.Trim().Length != 0)
+                isJoinValid = DateTime.TryParse(joinedAtInput, out res);
+        } while (!isJoinValid);
+
+        return res;
+    }
+
+    private string AskName()
+    {
+        var res = "";
+        do
+        {
+            Console.WriteLine("Enter the name of the Client : ");
+            res = Console.ReadLine();
+        } while (!IsNameValid(res));
+
+        return res;
+    }
+
     private bool IsNameValid(string? nameInput)
     {
         return nameInput != null && nameInput.Trim().Length > 3;
+    }
+
+    private void NewAccount()
+    {
+        var clientId = AskClientId();
+        var balance = AskBalance();
+        var res = AccountManager.CreateAccount(clientId, balance);
+        if (res == -1)
+            Console.WriteLine("An error occurred- Account not created");
+        else
+        {
+            Console.WriteLine("New Account created : " + res);
+        }
+
+        Console.ReadLine();
+        DisplayAccounts();
+    }
+
+    private double AskBalance()
+    {
+        bool isDouble;
+        double balance;
+        do
+        {
+            Console.WriteLine("Enter the balance of the account : ");
+            var inputBalance = Console.ReadLine();
+            isDouble = double.TryParse(inputBalance, out balance);
+        } while (!isDouble || balance < 0 || balance > 100000000000);
+
+        return balance;
+    }
+
+    private int AskClientId()
+    {
+        var inputId = "";
+        do
+        {
+            Console.WriteLine("Enter the id of the account's owner (client Id) : ");
+            inputId = Console.ReadLine();
+        } while (!IsValidClientId(inputId));
+
+        return int.Parse(inputId);
+    }
+
+    private bool IsValidClientId(string? inputId)
+    {
+        var res = false;
+        if (inputId != null && int.TryParse(inputId, out var id))
+            res = ClientRepo.Clients.Exists(c => c.Id == id);
+        return res;
     }
 }
